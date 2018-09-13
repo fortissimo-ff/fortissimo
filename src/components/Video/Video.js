8aeea2969ced88d908b4d958c80de59e58baac8e
Drew Ripberger <drew.ripberger@gmail.com>
2018-09-12 22:06:55 -0400

    Added the video component

---
+++ src/components/Video/Video.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { WebView } from 'react-native';
+import { Dimensions } from 'react-native';
+
+let width = Dimensions.get('window').width;
+
+class Video extends Component {
+  render() {
+    return (
+        <View style={styles.container}>
+            <WebView
+                source={{uri: 'https://www.youtube.com/embed/79XsGVCmNCo'}}
+                style={styles.video}
+            />
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 300,
+        width,
+    }
+});
+
+export default Video;
